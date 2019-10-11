3a575603056dd08624050718e9a5b35c143a9bf5
giangnv-0040 <49383891+giangnv-0040@users.noreply.github.com>
2019-10-11 19:16:41 +0700

    Custom layout (#2)
    * Create authenticate
    
    * Ignore some folder
    
    * Create custom layout

---
+++ resources/views/layouts/includes/header.blade.php
@@ -0,0 +1,26 @@
+<head>
+    <title>Winkel - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href="/theme/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="/theme/css/animate.css">
+
+    <link rel="stylesheet" href="/theme/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/theme/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/theme/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="/theme/css/aos.css">
+
+    <link rel="stylesheet" href="/theme/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="/theme/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="/theme/css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="/theme/css/flaticon.css">
+    <link rel="stylesheet" href="/theme/css/icomoon.css">
+    <link rel="stylesheet" href="/theme/css/style.css">
+</head>
